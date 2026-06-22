78a2d8fb1f5fd95df25b5db1bd558bbcb01d16c3
Miguel <miguel.benet@innoarea.com>
2026-06-22 21:21:39 +0200

    Upgrade versions and dont break the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    // id("kotlin-android") // Removed for Built-in Kotlin
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
