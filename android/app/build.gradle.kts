fed60e2b950b09038860bc592e4c9a29413a9802
Miguel <miguel.benet@innoarea.com>
2026-06-24 22:07:27 +0200

    gradlew bundleRelease fix and end to end view alert fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,6 +29,12 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    packaging {
+        jniLibs {
+            keepDebugSymbols += setOf("**/*.so")
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.musca.app"
